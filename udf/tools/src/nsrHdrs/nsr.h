6715534ef61c5bc9f8bdbdcd1ad33e9d05fc4a24
Ben Fennema <bfennema@falcon.csc.calpoly.edu>
1999-11-23 07:25:40 +0000

    UDF 0.8.9.1

--- udf/tools/src/nsrHdrs/nsr.h
+++ udf/tools/src/nsrHdrs/nsr.h
@@ -36,9 +36,9 @@
 /********************************************************************/
 /*
  $Source: /home/steve/src/linux-udf/udf/tools/src/nsrHdrs/nsr.h,v $
- $Revision: 1.21 $      $Author: bfennema $
+ $Revision: 1.22 $      $Author: bfennema $
  $State: Exp $          $Locker:  $
- $Date: 1999-11-23 07:15:45 $
+ $Date: 1999-11-23 07:25:40 $
  ********************************************************************/
 
 #include "nsr_sys.h"
