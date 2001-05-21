221df49146f255e8e57344c304829b387dd01ebe
Ben Fennema <bfennema@falcon.csc.calpoly.edu>
2001-05-21 06:05:54 +0000

    added UDF_RELOCATE_BLOCKS ioctl

--- udf/include/linux/udf_fs_i.h
+++ udf/include/linux/udf_fs_i.h
@@ -55,5 +55,6 @@ struct udf_inode_info
 #define UDF_GETEASIZE   _IOR('l', 0x40, int)
 #define UDF_GETEABLOCK  _IOR('l', 0x41, void *)
 #define UDF_GETVOLIDENT _IOR('l', 0x42, void *)
+#define UDF_RELOCATE_BLOCKS _IOWR('l', 0x43, long)
 
 #endif /* !defined(_LINUX_UDF_FS_I_H) */
