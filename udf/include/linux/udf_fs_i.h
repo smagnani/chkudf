b46abec732398507b6b6c0368c17d7c58b39a80e
Ben Fennema <bfennema@falcon.csc.calpoly.edu>
2002-08-06 22:57:52 +0000

    variable name changes

--- udf/include/linux/udf_fs_i.h
+++ udf/include/linux/udf_fs_i.h
@@ -56,9 +56,9 @@ struct udf_inode_info
 	__u32			i_next_alloc_block;
 	__u32			i_next_alloc_goal;
 	unsigned		i_alloc_type : 3;
-	unsigned		i_extended_fe : 1;
-	unsigned		i_strat_4096 : 1;
-	unsigned		i_new_inode : 1;
+	unsigned		i_efe : 1;
+	unsigned		i_use : 1;
+	unsigned		i_strat4096 : 1;
 	unsigned		reserved : 26;
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2,5,0)
 	struct inode vfs_inode;
